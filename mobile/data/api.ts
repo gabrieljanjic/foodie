27e951ea9dfce78d3b2bdcae74fef601d9eb2b9a
Gabriel Janjic <janjicgabriel1@gmail.com>
2026-03-06 17:38:03 +0100

    backend url

--- mobile/data/api.ts
+++ mobile/data/api.ts
@@ -1,5 +1,5 @@
 //const API_URL = "http://10.0.2.2:3000";
-const API_URL = "http://192.168.100.7:3000";
-//const API_URL = "https://foodie-backend-yg2l.onrender.com";
+//const API_URL = "http://192.168.100.7:3000";
+const API_URL = "https://foodie-backend-yg2l.onrender.com";
 
 export default API_URL;
