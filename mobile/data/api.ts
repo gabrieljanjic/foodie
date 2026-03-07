72728793aafade1bb50c46237ce35bc95aa2f669
Gabriel Janjic <janjicgabriel1@gmail.com>
2026-03-07 12:46:32 +0100

    README

--- mobile/data/api.ts
+++ mobile/data/api.ts
@@ -1,5 +1,5 @@
 //const API_URL = "http://10.0.2.2:3000"; //Simulator
-const API_URL = "http://192.168.100.7:3000"; //Mobile
-//const API_URL = "https://foodie-backend-yg2l.onrender.com";  //Render
+//const API_URL = "http://192.168.100.7:3000"; //Mobile
+const API_URL = "https://foodie-backend-yg2l.onrender.com"; //Render
 
 export default API_URL;
